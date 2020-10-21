72a7d438400761b631b51f857210ee429bc755b8
gitRedgieGravador <iderge32@gmail.com>
2020-10-21 20:30:25 +0800

    test

--- controllers/test.js
+++ controllers/test.js
@@ -1,25 +1,24 @@
 const MySql = require("../mysql");
 var mysql = new MySql();
 module.exports = {
-  get(req, res) {
-    /* change table name and database to get a success result. */
+  getTest(req, res) {
     mysql.query("select * from test", resp => {
       res.json(resp);
     });
   },
-  update(req, res) {
+  updateTest(req, res) {
     res.json({
       message: "updating..",
       data: req.body
     });
   },
-  create(req, res) {
+  createTest(req, res) {
     res.json({
       message: "creating..",
       data: req.body
     });
   },
-  delete(req, res) {
+  deleteTest(req, res) {
     res.json("delete test.");
   }
 }
--- routes/test.router.js
+++ routes/test.router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const TEST = require("../services/test")
+const TEST = require("../controllers/test")
 const AUTH = require('../middleware/authorization');
 
-router.post('/test-get', TEST.get)
-router.post('/test-create', TEST.create)
-router.post('/test-update', TEST.update)
-router.post('/test-delete', TEST.delete)
+router.get('/tests', /* AUTH.authorized, */ TEST.getTest)
+router.post('/tests', TEST.createTest)
+router.put('/tests', TEST.updateTest)
+router.delete('/tests', TEST.deleteTest)
 
 module.exports = router;
